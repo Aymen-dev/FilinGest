b382dfbd747956913b0716c24f6197133590df43
Aymen <benmariema@gmail.com>
2024-03-19 18:40:50 +0100

    display changes of date and equipe

--- src/app/view-details-production/view-details-production.component.ts
+++ src/app/view-details-production/view-details-production.component.ts
@@ -5,6 +5,8 @@ import { Machine } from '../models/machine.model';
 import { DetailsProduction } from '../models/details-production.model';
 import { EnteteProduction } from '../models/entete-production.model';
 import { DepartementService } from '../services/departement.service';
+import { DateService } from '../services/date.service';
+import { Equipe } from '../models/equipe.model';
 
 @Component({
   selector: 'app-view-details-production',
@@ -20,8 +22,10 @@ export class ViewDetailsProductionComponent implements OnInit {
   machines?: Array<Machine>;
   details?: Array<DetailsProduction>;
   entete?: EnteteProduction;
+  equipe?: Equipe;
+  date: string = '';
 
-  constructor(private depService: DepartementService) {
+  constructor(private depService: DepartementService, private dateService: DateService) {
   }
 
   ngOnInit(): void {
@@ -41,6 +45,8 @@ export class ViewDetailsProductionComponent implements OnInit {
     this.machines = this.response.data.machines;
     this.details = this.response.data.detailsProduction;
     this.entete = this.response.data.enteteProduction;
+    this.equipe = this.response.data.equipe;
+    this.date = this.dateService.formatDate(new Date(this.entete?.date_production!));
   }
 
 }
