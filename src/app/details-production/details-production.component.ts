2b52e929a0ce18d4028c595b7f5615f56ea0c92e
Aymen <benmariema@gmail.com>
2024-04-20 16:19:21 +0100

    titres mriglin bel api jdid

--- src/app/details-production/details-production.component.ts
+++ src/app/details-production/details-production.component.ts
@@ -12,6 +12,8 @@ import { Departement } from '../models/departement.model';
 import { DepartementService } from '../services/departement.service';
 import { Equipe } from '../models/equipe.model';
 import { EquipeService } from '../services/equipe.service';
+import { PlanificationService } from '../services/planification.service';
+import { NgForm } from '@angular/forms';
 
 
 
@@ -28,7 +30,7 @@ export class DetailsProductionComponent implements OnInit {
   dateProduction = '';
   titresFil: Array<TitreFil> = [];
   inputLevata: Array<number> = [];
-  inputTitre: Array<number> = [];
+  inputTitre: Array<number | null> = [];
   nbLevatas: Array<number> = [];
   response!: BackendResponse;
   isDepFilature: boolean = false;
@@ -50,14 +52,44 @@ export class DetailsProductionComponent implements OnInit {
     this.response = history.state.response;
     this.getEntities();
     this.setInputLevata();
+    this.setInputTitres();
   }
 
 
   constructor(private route: ActivatedRoute, private router: Router,
     private machineService: MachineService, private prodService: ProductionService,
     private titreService: TitreFilService, private popUpService: PopUpService,
-    private depService: DepartementService, private equipeService: EquipeService) {
+    private depService: DepartementService, private equipeService: EquipeService,
+    private planificationService: PlanificationService) {
 
+  }
+
+  setInputTitres() {
+    this.planificationService.getPlans({
+      date: this.enteteProdData ? this.enteteProdData.date_production : this.response.data.enteteProduction?.date_production,
+      dep: this.enteteProdData ? this.enteteProdData.idDepartement : this.response.data.departement?.id_departement
+    }).subscribe({
+      next: response => {
+        this.titreService.getTitresFil().subscribe({
+          next: resp => {
+            this.titresFil = resp.data.titresFil!
+            if (history.state.action == 'add')
+              for (let item of (response as any).data.plans) {
+                if (item.plan.length == 0)
+                  this.inputTitre.push(null);
+                else
+                  this.inputTitre.push(this.titresFil.find(element => element.id_titre == item.plan[0].titre)!.id_titre)
+              }
+            else
+              for (let item of this.response.data.detailsProduction!)
+                this.inputTitre.push(item.titre_fil!);
+          },
+          error: err => {
+            console.log(err);
+          }
+        });
+      }
+    })
   }
 
   getEntities() {
@@ -104,21 +136,12 @@ export class DetailsProductionComponent implements OnInit {
         console.log(err);
       }
     });
-
-    this.titreService.getTitresFil().subscribe({
-      next: response => {
-        this.titresFil = response.data.titresFil!
-      },
-      error: err => {
-        console.log(err);
-      }
-    });
   }
 
 
 
   setInputLevata() {
-    if (this.response)
+    if (this.response) {
       if (this.response.data.detailsProduction) {
         const detailsProduction = this.response.data.detailsProduction as DetailsProduction[];
 
@@ -130,57 +153,64 @@ export class DetailsProductionComponent implements OnInit {
           this.inputLevata.push(nb);
         });
       }
-
+    }
   }
 
-
-  ajouterPanne() {
-
-  }
 
   setFormData(response: BackendResponse): void {
     this.machines.forEach(machine => {
-      const productionDetail = {
+      const productionDetail: {
+        nbLevata: number,
+        id_entete: number,
+        numero_machine: number,
+        systeme: number,
+        titre: number | null
+      } = {
         nbLevata: this.inputLevata[this.machines!.indexOf(machine)],
-        id_entete: response.data.enteteProduction?.id_entete,
+        id_entete: response.data.enteteProduction!.id_entete,
         numero_machine: machine.numero,
         systeme: machine.systeme,
-        titre: this.inputTitre.length != 0 ? this.inputTitre[this.machines!.indexOf(machine)] : null
+        titre: this.inputTitre.length !== 0 ? this.inputTitre[this.machines!.indexOf(machine)] : null
       };
       this.formData.productionDetails.push(productionDetail);
     });
   }
 
-  createDetailsProduction(formData: any) {
+  createDetailsProduction(formData: any, myForm: NgForm) {
     console.log(this.enteteProdData);
-    console.log(history.state)
-    if (history.state.action == 'add')
-      this.prodService.saveEnteteProduction(this.enteteProdData).subscribe({
-        next: (response: BackendResponse) => {
-          this.setFormData(response);
-          console.log(this.formData)
-          this.prodService.saveDetailsProduction(this.formData).subscribe(
-            response => {
-              this.popUpService.showSuccess(response.message);
-              this.router.navigate(['liste-productions']);
-            }
-          )
-        },
-        error: err => {
-          console.error(err);
-          this.popUpService.showFail("Erreur serveur");
-        }
-      });
+    console.log(history.state);
+    console.log(myForm)
+    if (!myForm.valid)
+      this.popUpService.showFail("Tous les details sont requises")
     else {
-      this.setFormData(history.state.response);
-      console.log(this.formData)
-      this.prodService.editDetailsProduction(this.formData, this.response.data.enteteProduction!.id_entete).subscribe(
-        response => {
-          console.log(history.state.entete)
-          this.popUpService.showSuccess(response.message);
-          this.router.navigate(['liste-productions']);
-        }
-      )
+      if (history.state.action == 'add')
+        this.prodService.saveEnteteProduction(this.enteteProdData).subscribe({
+          next: (response: BackendResponse) => {
+            this.setFormData(response);
+            console.log(this.formData)
+            this.prodService.saveDetailsProduction(this.formData).subscribe(
+              response => {
+                this.popUpService.showSuccess(response.message);
+                this.router.navigate(['liste-productions']);
+              }
+            )
+          },
+          error: err => {
+            console.error(err);
+            this.popUpService.showFail("Erreur serveur");
+          }
+        });
+      else {
+        this.setFormData(history.state.response);
+        console.log(this.formData)
+        this.prodService.editDetailsProduction(this.formData, this.response.data.enteteProduction!.id_entete).subscribe(
+          response => {
+            console.log(response)
+            this.popUpService.showSuccess(response.message);
+            this.router.navigate(['liste-productions']);
+          }
+        )
+      }
     }
   }
 }
